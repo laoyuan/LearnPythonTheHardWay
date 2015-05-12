8c15992734c6a3f2e2b6ff13b272336b4ec56459
Laoyuan <bitlaoyuan@youremail.com>
2015-05-12 08:01:47 +0800

    20150512

--- ex33.py
+++ ex33.py
@@ -1,19 +1,19 @@
 
 def cc(times, add):
-	i = 0
-	num = 0
-	numbers = []
+    i = 0
+    num = 0
+    numbers = []
 
-	for i in range(times):
-		print "At the top i is %d" % num
-		numbers.append(num)
-		num = num + add
-		print "Numbers now: ", numbers
-		print "At the bottom i is %d" % num
+    for i in range(times):
+        print "At the top i is %d" % num
+        numbers.append(num)
+        num = num + add
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % num
 
-	print "The numbers: "
-	for num in numbers:
-	    print num
+    print "The numbers: "
+    for num in numbers:
+        print num
 
 
 cc(3, 2)
